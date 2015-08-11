205b9e2c755fb68490be338569d279f40b044db2
Tommi Enenkel <tommi.enenkel@gmail.com>
2015-08-11 23:46:59 +0200

    add comments to response plugin

--- lib/plugins/response_plugin.dart
+++ lib/plugins/response_plugin.dart
@@ -2,14 +2,23 @@ library slack_bot_response_plugin;
 
 import "package:slack_bot/slack_bot.dart";
 
+/**
+ * Responds to phrases that other users say with our phrases.
+ *
+ * ToDo: store responses into a file so that users can add their own responses
+ */
 class ResponsePlugin extends IPlugin {
 
-  Map responses;
+  Map _responses;
 
+  /**
+   * The constructor prefills our list of responses
+   */
   ResponsePlugin(){
     int mps_in_team_stronach = 6;
 
-    responses = {
+    // our list of reponses
+    _responses = {
       "Erneuerung": "Erneuerung!",
       "wuzeln?": "Der Slackbot ist ja sooo toll...",
       "Stronach": "Habt's ihr gewusst, dass das Team Stronach immer noch $mps_in_team_stronach Abgeordnete hat?",
@@ -17,22 +26,32 @@ class ResponsePlugin extends IPlugin {
     };
   }
 
+  /**
+   * Decides if it should response to the message posted to the channel
+   *
+   * Returns true if it did respond to the message.
+   */
   bool respond(Map message){
 
+    // we only look into text messages
     if(message["type"] == "message") {
       String text = message["text"];
 
+      // if it is not a text message, ignore it
       if(text != null){
 
-        String key = responses.keys.firstWhere((String element) => text.toLowerCase().contains(element.toLowerCase()), orElse:()=>null);
+        // compare all phrases in _responses with the text. if the text contains one of our phrases, this is the key
+        String key = _responses.keys.firstWhere((String element) => text.toLowerCase().contains(element.toLowerCase()), orElse:()=>null);
+
+        // if we got a hit, take this response and post it back to the channel from which we received our message
         if(key != null){
-          client.postMessage(responses[key], message["channel"]);
-          return true;
+          client.postMessage(_responses[key], message["channel"]);
+          return true; // return true because we handled the message
         }
       }
     }
 
-    return false;
+    return false; // return false because we didnt handle the message
 
   }
 }
